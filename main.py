f92749017d8f5b9985d738965194b5827a1dee8c
Shadow <borntodevelop77@gmail.com>
2024-01-09 23:31:31 +0530

    some inverted commas issue

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ def main():
                 # Step 1: Extract audio from the movie
                 #ffmpeg -i movie.mkv -vn -q:a 0 -map a movie.mp3
                 extracted_mp3_audio = (row["movie_list"]).split('.')[0]
-                command = f'ffmpeg -i "{movie_directory}\{row["movie_list"]}" -vn -q:a 0 -map a "{export_directory}\exported_audio\{extracted_mp3_audio}.mp3"'
-                print(command)
+                command = f'ffmpeg -i "{movie_directory}\{row["movie_list"]}" -vn -q:a 0 -map a "{export_directory}\exported_audio\{extracted_mp3_audio}.mp3" -y'
+                # print(command+'\n')
                 run_command(command)
 
                 # Step 2: Prepare the movie audio for overlapping
@@ -51,8 +51,9 @@ def main():
                 for x in range(len(audio_files)):
                     volume_enable = volume_enable + f"volume=enable='between(t,{row[f'claim_{x}']*60},{(row[f'claim_{x}']*60)+120})':volume=0, "
                 
-                command_2 = f'''ffmpeg -i {export_directory}\exported_audio\{extracted_mp3_audio}.mp3 -af "{volume_enable}" "{export_directory}\exported_audio\{movie_silenced}"'''
+                command_2 = f'''ffmpeg -i {export_directory}\exported_audio\{extracted_mp3_audio}.mp3 -af "{volume_enable}" "{export_directory}\exported_audio\{movie_silenced}" -y'''
                 run_command(command_2)
+                # print(command_2+'\n')
 
                 # Step 3: Overlap the additional audio files onto the movie audio
                 #millisecond-minutes conversion
@@ -62,21 +63,23 @@ def main():
                 aud = '[0:a]'
                 for x in range(len(audio_files)):
                     adelay = adelay + f"[{x+1}:a]adelay={row[f'claim_{x}']*60000}|{row[f'claim_{x}']*60000}[aud{x+1}]; "
-                    audio_input = audio_input + f"-i '{audio_files[x]}' "
+                    audio_input = audio_input + f'-i "{audio_files[x]}" '
                     aud = aud + f"[aud{x+1}]"
                 
                 movie_output_mp3 = f"{export_directory}\exported_audio\{row['movie_list'].split('.')[0]}_output.mp3"
-                command_3 = f'''ffmpeg -i "{export_directory}\exported_audio\{movie_silenced}" {audio_input} -filter_complex "{adelay}amix=inputs={len(audio_files)}:duration=longest:dropout_transition=0" {movie_output_mp3}'''
+                command_3 = f'''ffmpeg -i "{export_directory}\exported_audio\{movie_silenced}" {audio_input}-filter_complex "{adelay}{aud}amix=inputs={len(audio_files)+1}:duration=first:dropout_transition=0" -c:a libmp3lame {movie_output_mp3} -y'''
 
                 run_command(command_3)
+                # print(command_3+'\n')
 
                 
                 # Step 4: Combine the new audio with the video
                 #run_command(['ffmpeg', '-i', 'movie.mp4', '-i', 'output.mp3', '-c:v', 'copy', '-map', '0:v:0', '-map', '1:a:0', '-shortest', 'final_movie.mp4'])
                 movie_output = f"{export_directory}\{row['movie_list'].split('.')[0]}.mp4"
-                command_4 = f'''ffmpeg -i '{movie_directory}\{row['movie_list']}' -i {movie_output_mp3} -c:v copy -map 0:v:0 -map 1:a:0 -shortest {movie_output}'''
+                command_4 = f'''ffmpeg -i {movie_directory}\{row['movie_list']} -i {movie_output_mp3} -c:v copy -map 0:v:0 -map 1:a:0 -shortest {movie_output} -y'''
 
                 run_command(command_4)
+                # print(command_4)
                 
     except Exception as e:
         print(f"An error occurred: {e}")
